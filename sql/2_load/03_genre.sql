69c97e0799a34cfa995eb5b441a7a769a75e62ca
itu-itis23-genelz22 <genelz22@itu.edu.tr>
2025-11-09 01:15:53 +0300

    Week 6 commit
    + Created 'genres.csv' dataset file and placed it under the /data folder.
    +Created 'Genre' table schema in schema/00_genres_raw.sql
    +Added "03_genre.sql" under "02_load" folder that load its non keys from genres_raw
    +Updated genres data loading script in master_build

---
+++ sql/2_load/03_genre.sql
@@ -0,0 +1,16 @@
+-- Responsibility: Zeynep Nur Genel
+-- Loading data to Genre Table --
+USE databeez_db;
+
+INSERT INTO Genre (
+    Genre_Name, 
+    Description, 
+    Example_Game)
+
+SELECT
+    t.Genre_Name, 
+    t.Description, 
+    t.Example_Game
+FROM    
+    genres_raw t;
+
